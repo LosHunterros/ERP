024e8b942d732ff2d20e35dda0afd5368d939b88
LosHunterros <m.piech@piechdesign.pl>
2023-03-17 19:45:30 +0100

    Function to generate ID

--- model/util.py
+++ model/util.py
@@ -7,7 +7,29 @@ def generate_id(number_of_small_letters=4,
                 number_of_digits=2,
                 number_of_special_chars=2,
                 allowed_special_chars=r"_+-!"):
-    return 'T!uq6-b4Yq'
+
+    length = number_of_small_letters + number_of_capital_letters + number_of_digits + number_of_special_chars
+    id = list(range(length))
+    random_positions = []
+
+    while len(random_positions) < length:
+        random_number = random.randint(0,length-1)
+        if random_number not in random_positions: random_positions.append(random_number)
+
+    for i in range(number_of_small_letters):
+        id[random_positions[0]] = random.choice(string.ascii_lowercase)
+        del random_positions[0]
+    for i in range(number_of_capital_letters):
+        id[random_positions[0]] = random.choice(string.ascii_uppercase)
+        del random_positions[0]
+    for i in range(number_of_digits):
+        id[random_positions[0]] = random.choice(string.digits)
+        del random_positions[0]
+    for i in range(number_of_special_chars):
+        id[random_positions[0]] = random.choice(allowed_special_chars)
+        del random_positions[0]
+
+    return "".join(id)
 
 def convert_to_boolean(data):
     if data == "Y": return "1"
